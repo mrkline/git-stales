ae3e7937e156d033ab110126577754d38cc13087
Matt Kline <matthew.kline@flukenetworks.com>
2015-10-15 13:38:19 -0700

    Proof of concept

---
+++ Makefile
@@ -0,0 +1,18 @@
+DC = dmd
+DFLAGS = -w -wi
+
+all: debug
+
+debug: DFLAGS += -debug -unittest -g
+debug: stales
+
+release: DFLAGS += -release -O
+release: stales
+
+stales: stales.d
+	$(DC) $(DFLAGS) -of$@ $^
+
+clean:
+	rm -f stales *.o
+
+.PHONY: all, clean, debug, release
---
+++ stales.d
@@ -0,0 +1,88 @@
+import std.stdio;
+import std.conv : to;
+import std.process;
+import std.algorithm;
+import std.exception : enforce;
+import std.string : strip, stripLeft, indexOf;
+import std.array : array;
+import std.typecons : tuple;
+import std.datetime;
+
+/// Returns a range of branches that are on both remotes
+auto findRemoteBranches()
+{
+    // -r lists remote branches
+    auto remoteBranchFinder = pipeProcess(["git", "branch", "-r"], Redirect.stdout);
+    // Make sure the process dies with us
+    scope (failure) kill(remoteBranchFinder.pid);
+    scope (exit) {
+        enforce(wait(remoteBranchFinder.pid) == 0, "git branch -r failed");
+    }
+
+    auto remoteBranches = remoteBranchFinder.stdout
+        .byLine()
+        // Filter out tracking branches (e.g. origin/HEAD -> origin/something)
+        .filter!(b => !b.canFind("->"))
+        // git branch -r puts whitespace on the left. Strip that.
+        .map!(b => stripLeft(b))
+        // Allocate a new string for each line (byLine reuses a buffer)
+        .map!(m => m.idup);
+
+    return remoteBranches.array;
+}
+
+/// Takes a branch name string and strips the remote off the front
+auto splitRemoteNameFromBranch(string branch) pure
+{
+    immutable firstSlash = branch.indexOf('/');
+    return tuple!("remote", "branch")(
+        branch[0 .. firstSlash],
+        branch[firstSlash + 1 .. $]);
+}
+
+Duration ageOfBranch(string branch)
+{
+    auto showResult = execute(["git", "show", "-s", "--format=%cI", branch]);
+    enforce(showResult.status == 0, "git show failed on branch " ~ branch);
+    immutable iso = showResult.output.strip();
+    immutable branchDate = SysTime.fromISOExtString(iso);
+    return Clock.currTime - branchDate;
+}
+
+// Gives how many commits branch is ahead and behind upstream
+auto aheadBehindCounts(string branch, string upstream = "master")
+{
+    auto revListResult = execute(
+        ["git", "rev-list", "--left-right", "--count", branch ~ "..." ~ upstream]);
+    enforce(revListResult.status == 0,
+            "git rev-list failed on branches " ~ branch ~ " and " ~ upstream);
+
+    // The above rev-list command gives <commits ahead><tab><commits behind>
+    auto counts = revListResult.output.strip().splitter('\t');
+    string ahead = counts.front;
+    counts.popFront();
+    string behind = counts.front;
+    counts.popFront();
+    enforce(counts.empty, "Unexpected git rev-list output");
+
+    return tuple!("ahead", "behind")(ahead.to!int, behind.to!int);
+}
+
+int main()
+{
+    foreach (branch; findRemoteBranches()) {
+        immutable counts = aheadBehindCounts(branch);
+        // Ignore unmerged branches
+        if (counts.ahead > 0)
+            continue;
+
+        // Ignore younger branches
+        immutable ageInDays = ageOfBranch(branch).total!"days";
+        if (ageInDays < 30)
+            continue;
+
+        writeln(branch, " is ", ageInDays, " days old and ",
+                counts.behind, " commits behind master.");
+    }
+    return 0;
+}
